cb096ac6a15d036584756deaa675911afc60f3e9
thanhvk <thanhvk.w3s@gmail.com>
2022-03-22 16:18:26 +0700

    function with return values

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
     // print_labeled_measurement(5, 'h');
-    statement_expression();
+    // statement_expression();
+    // let x = five();
+    let x = plus_one(5);
+
+    println!("The value of x is: {}", x);
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
@@ -14,4 +18,12 @@ fn statement_expression() {
   };
 
   println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+  5
+}
+
+fn plus_one(x: i32) -> i32 {
+  x + 1
 }
