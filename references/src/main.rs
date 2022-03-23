7381d64a244d8598ad559df3a7dccef1f2968f01
thanhvk <thanhvk.w3s@gmail.com>
2022-03-23 15:34:52 +0700

    end reference

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,11 +1,18 @@
 fn main() {
-  let s1 = String::from("hello");
+  let mut s1 = String::from("hello");
+  println!("s1 before: {}", s1);
 
-  let len = calculate_length(&s1);
+  // let len = calculate_length(&s1);
 
-  println!("The length of '{}' is {}.", s1, len);
+  // println!("The length of '{}' is {}.", s1, len);
+  change(&mut s1);
+  println!("s1 after: {}", s1);
 }
 
 fn calculate_length(s: &String) -> usize {
   s.len()
+}
+
+fn change(some_string: &mut String) {
+  some_string.push_str(", world");
 }
