7db5f0109217ffb5ac7e9454a86c44e3a102ac4a
thanhvk <thanhvk.w3s@gmail.com>
2022-03-23 14:16:10 +0700

    pointer

---
+++ ownership/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+  // let mut s = String::from("hello");
+
+  // s.push_str(", world!"); // push_str() appends a literal to a String
+
+  // println!("{}", s); // This will print `hello, world!`
+
+  let s1 = String::from("hello");
+  let s2 = s1;
+
+  println!("{}, world!", s1);
+}
