4ccd46722f218a36526047ebeb6d1ba4340bb7f9
thanhvk <thanhvk.w3s@gmail.com>
2022-03-23 14:56:39 +0700

    ownership example

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,17 +1,14 @@
 fn main() {
-  // let mut s = String::from("hello");
-
-  // s.push_str(", world!"); // push_str() appends a literal to a String
-
-  // println!("{}", s); // This will print `hello, world!`
-
-  // let s1 = String::from("hello");
-  // let s2 = s1;
-
-  // println!("{}, world!", s1);
-
   let s1 = String::from("hello");
-  let s2 = s1.clone();
 
-  println!("s1 = {}, s2 = {}", s1, s2);
+  let (s2, len) = calculate_length(s1);
+  println!("s1 {}", s1);
+
+  println!("The length of '{}' is {}.", s2, len);
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+  let length = s.len(); // len() returns the length of a String
+
+  (s, length)
 }
